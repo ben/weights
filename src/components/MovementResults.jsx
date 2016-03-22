9009c235748da0b97d8a856589873b2a82b63bfc
Ben Straub <ben@straub.cc>
2016-03-22 11:57:09 -0700

    Enhance search-result row

--- src/components/MovementResults.jsx
+++ src/components/MovementResults.jsx
@@ -12,7 +12,8 @@ class MovementResults extends React.Component {
     // TODO: Get state from localstorage
     this.setState({
       movements: [
-        {movement: 'deadlift', weight: 225, reps: 3, notes: 'PR'}
+        {movement: 'deadlift', weight: 225, reps: 3, recorded: new Date(2016, 1, 1, 13, 15)},
+        {movement: 'deadlift', weight: 225, reps: 5, recorded: new Date(2016, 1, 2, 13, 15)}
       ]
     });
   }
@@ -21,27 +22,47 @@ class MovementResults extends React.Component {
     if (this.props.query === '') {
       return <div/>
     }
-    var re = new RegExp(this.props.query);
+    var re = new RegExp(this.props.query, 'i');
     var results = this.state.movements.filter(function (x) {
       return re.test(x.movement);
+    }).sort(function (x) {
+      return x.recorded;
     });
 
     if (results.length === 0) {
       return (<div />);
     }
 
+    var style={
+      width: '35px',
+      padding: 0
+    }
+
     return (
-      <table>
+      <table className="u-full-width">
+        <col />
+        <col />
+        <col />
+        <col />
+        <col width="35px" />
         <thead>
           <tr>
+            <th>When</th>
             <th>Movement</th>
+            <th>Weight</th>
+            <th>Reps</th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
           {results.map(function (r) {
             return (
               <tr>
+                <td>{r.recorded.toLocaleString()}</td>
                 <td>{r.movement}</td>
+                <td>{r.weight}</td>
+                <td>{r.reps}</td>
+                <td><button style={style}>+</button></td>
               </tr>
             )
           })}
